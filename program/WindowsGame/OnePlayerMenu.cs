dd50cdaab61317c61717530f59dba18206c4f368
AntonDarkness <159625557+AntonDarkness@users.noreply.github.com>
2024-02-10 21:31:09 +0300

    Create OnePlayerMenu.cs
    Create OnePlayerMenu.cs

---
+++ program/WindowsGame/OnePlayerMenu.cs
@@ -0,0 +1,15 @@
+public class OnePlayerMenu
+{
+  /*некоторые параметры*/
+  
+  public OnePlayerMenu(){
+    //загрузка окна одиночной игры
+  }
+  public void buttonExit(){
+    //обработка нажатия кнопки "назад"
+  }
+  public void buttonStartGame(){
+    //обработка нажатия кнопки "начать игру"
+  }
+  //другие методы
+}

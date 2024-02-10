b68ed2867cb759aaf36137af2963aaddfe5a8d7a
AntonDarkness <159625557+AntonDarkness@users.noreply.github.com>
2024-02-10 21:36:53 +0300

    Create MultiplayerMenu.cs
    Create MultiplayerMenu.cs

---
+++ program/WindowsGame/MultiplayerMenu.cs
@@ -0,0 +1,29 @@
+public class MultiplayerMenu
+{
+  private bool statusPlayer;
+  private bool musicOn;
+  private bool musicEffectsOn;
+  private bool accountPerson;
+  private string ipAdress;
+  private Account person;
+
+  public MultiplayerMenu(){
+    //загрузка интерфейса многопользовательской игры
+  }
+  public void loadPersonAccount(){
+    /*Метод в котором проверяется наличие аккаунта у пользователя.
+    Если аккаунт есть - происходит его загрузка, 
+    а если аккаунт нет - происходит выбор:
+    создать аккаунт или играть как гость*/
+  }
+  public void buttonExit(){
+    //метод обработки нажатия кнопки "назад"
+  }
+  public void buttonStartGame(){
+    //метод обработки нажатия кнопки "поиск игры"
+  }
+  public bool checkInternetConnection(){
+    //метод проверки интернет соединения
+  }
+  //другие методы
+}

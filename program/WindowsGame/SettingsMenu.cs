431d0a34eb11bf04632dea66f3bdee7bfe5695a3
AntonDarkness <159625557+AntonDarkness@users.noreply.github.com>
2024-02-10 21:43:52 +0300

    Create SettingsMenu.cs
    Create settings menu file

---
+++ program/WindowsGame/SettingsMenu.cs
@@ -0,0 +1,22 @@
+public class SettingsMenu
+{
+  /*параметры настроек, которые подлежат изменениям*/
+
+  public SettingsMenu(){
+    //Загрузка интерфейса настроек
+  }
+
+  public void changedMusicStatus(){
+    //изменение параметров музыки
+  }
+  public void changedMusicEffectsStatus(){
+    //изменение параметров музыкальных истоков
+  }
+
+  public void buttonExit(){
+    //кнопка выхода из настроек
+  }
+  
+  //другие методы настроек
+  
+}

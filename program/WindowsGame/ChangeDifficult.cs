cd5e5fc3bdc90712e99ad78ae48c7df49ad95580
AntonDarkness <159625557+AntonDarkness@users.noreply.github.com>
2024-02-10 21:40:49 +0300

    Create ChangeDifficult.cs
    Create ChangeDifficult.cs

---
+++ program/WindowsGame/ChangeDifficult.cs
@@ -0,0 +1,22 @@
+public class ChangeDifficult
+{
+  private int tempDifficult;
+  private long maxRecord;
+
+  public ChangeDifficult(){
+    //загрузка окна изменения сложности игры (только одиночная игра)
+  }
+  public void changedDifficult(){
+    //метод изменения сложности игры
+  }
+  public void changedSliderDifficult(){
+    //изменение ползунка сложности игры
+  }
+  public void buttonExit(){
+    //метод обработки нажатия кнопки "назад"
+  }
+  public void buttonStart(){
+    //обработка нажатия кнопки "Начать игру"
+  }
+  //другие методы
+}

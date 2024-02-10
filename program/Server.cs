b5507160f688927a5e60f71e06e6170fdf7327dd
AntonDarkness <159625557+AntonDarkness@users.noreply.github.com>
2024-02-10 20:12:44 +0300

    Create Server.cs
    Create server file

---
+++ program/Server.cs
@@ -0,0 +1,30 @@
+public class Server
+{
+  private long[] id_user;
+  private string[] username;
+
+  public Server(){
+    //выполняется запуск сервера
+  }
+  public void unloadServer(){
+    //выполняется выключение сервера
+  }
+  public void connectionClients(int user_id_1, int user_id_2){
+    //здесь происходит подключение клиентов к игре, а потом
+    //происходит создание игровой сессии
+  }
+  public void createGameSession(int user_id_1, int user_id_2){
+    //происходит создание игровой сессии между игроками между 2 игроками.
+    //Потом происходит запуск игрового процесса
+  }
+  public void createGameProcess(int user_id_1, int user_id_2){
+    //создание игрового поля и начало игрового процесса
+    //в случае проблем с интернетом выполняется синхронизация данных
+    //в случае выигрыша/или выхода одного из игроков - вывод результатов и уничтожение сессии
+  }
+  public void deleteGameSession(int user_id_1, int user_id_2){
+    //уничтожение игровой сесии
+  }
+  
+  /*Другие методы, необходимые для корректной работы сервера*/
+}

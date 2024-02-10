00d820497ac12100db2422a8dda680a877ca6559
AntonDarkness <159625557+AntonDarkness@users.noreply.github.com>
2024-02-10 21:55:02 +0300

    Create StartGame.cs
    Create StartGame.cs

---
+++ program/StartGame.cs
@@ -0,0 +1,15 @@
+public class StartGame
+{
+  private bool statusGame;
+  private bool musicOn;
+  private bool musicEffects;
+
+  public StartGame(){
+    //метод определяющий заставку во время загрузки игры
+  }
+
+  public void changedMenu(){
+    //метод загрузающий главное меню, после заставки
+  }
+  //некоторые другие методы
+}

f4a75689a214f52fbe5b2ce8c64503b082e2c1fb
AntonDarkness <159625557+AntonDarkness@users.noreply.github.com>
2024-03-01 11:31:18 +0300

    Update Account.cs

--- program/Account.cs
+++ program/Account.cs
@@ -1,17 +1,37 @@
 public class Account
 {
+  ///<summary>
+  ///Индетефикационный номер пользователя
+  ///</summary>
   private int userid;
+  ///<summary>
+  ///Имя пользователя
+  ///</summary>
   private username;
+  ///<summary>
+  ///IP-адресс пользователя
+  ///</summary>
   private long ipAdress;
-
+  ///<summary>
+  ///Конструктор класса для создания аккаунта пользователя
+  ///</summary>
   public Account(){
     //создание аккаунта пользователя
   }
+  ///<summary>
+  ///Метод подключения к серверу
+  ///</summary>
   public void conectionOnServer(){
     //подключение игрока к серверу
   }
+  ///<summary>
+  ///Метод изменения имени пользователя по указанному новому имени
+  ///</summary>
   public void changeUsername(string newName){
     //метод изменения никнейма пользователя
   }
+  ///<summary>
+  ///Другие методы, которые необходимы для корректной работы
+  ///</summary>
   /*другие методы*/
 }
